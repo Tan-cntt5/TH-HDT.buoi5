74a738cb4020016fc00be853153765a908cd4e12
Tan-cntt5 <125740918+Tan-cntt5@users.noreply.github.com>
2023-05-26 16:48:32 +0700

    Add files via upload

---
+++ PhongHocMayTinh.java
@@ -0,0 +1,58 @@
+package tuan4_M4_PhongHoc_KhongInterface;
+
+public class PhongHocMayTinh extends PhongHoc{
+	private int soLuongMayTinh;
+	public int getSoLuongMayTinh() {
+		return soLuongMayTinh;
+	}
+	public void setSoLuongMayTinh(int soLuongMayTinh) {
+		this.soLuongMayTinh = soLuongMayTinh;
+	}
+	/**
+	 * 
+	 */
+	public PhongHocMayTinh() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * @param maPhong
+	 * @param dayNha
+	 * @param dienTich
+	 * @param soBongDen
+	 */
+	public PhongHocMayTinh(String maPhong, String dayNha, double dienTich, int soBongDen) {
+		super(maPhong, dayNha, dienTich, soBongDen);
+		// TODO Auto-generated constructor stub
+	}
+	/**
+	 * @param maPhong
+	 * @param dayNha
+	 * @param dienTich
+	 * @param soBongDen
+	 * @param soLuongMayTinh
+	 */
+	public PhongHocMayTinh(String maPhong, String dayNha, double dienTich, int soBongDen, int soLuongMayTinh) {
+		super(maPhong, dayNha, dienTich, soBongDen);
+		this.soLuongMayTinh = soLuongMayTinh;
+	}
+	private float soChuanMayTinh() {
+		double soChuan = this.getDienTich() / 1.5;
+		return (float) soChuan;
+	}
+	
+	@Override
+	public boolean isDatChuan() {
+		float soMayTinhChuan = this.soChuanMayTinh();
+		if(super.phongDuAnhSang() == true && (soMayTinhChuan <= (float) this.getSoLuongMayTinh()) ) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	@Override
+	public String toString() {
+		
+		return super.toString()+String.format(" | %15s | %15d | %-25s | %15s | %-15s | %-20s", "--",this.getSoLuongMayTinh(),"--","--","--","Phòng Máy Tính");
+	}
+}
